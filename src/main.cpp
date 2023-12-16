4e7cc8db4dce5254326a7618ecc274d46a5bca78
Hrvoje <vhrvoje2@yahoo.com>
2023-12-16 14:29:18 +0100

    Add shape classes and rendering

--- src/main.cpp
+++ src/main.cpp
@@ -6,44 +6,178 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
 
+#define FRAMERATE 60
+#define DEFAULT_SEGMENTS 32
+
+class RGBColor
+{
+public:
+    int mRed;
+    int mGreen;
+    int mBlue;
+
+    RGBColor(){};
+    RGBColor(int red, int green, int blue)
+        : mRed(red),
+          mGreen(green),
+          mBlue(blue) {}
+};
+
+class CShape : RGBColor
+{
+public:
+    std::string mName;
+    float mInitialX;
+    float mInitialY;
+    float mSpeedX;
+    float mSpeedY;
+    bool mIsDrawn = true;
+    bool mIsTextDrawn = true;
+    sf::RectangleShape mSfmlRectangle;
+    sf::CircleShape mSfmlCircle;
+
+    CShape(std::string name, float initialX, float initialY, float speedX, float speedY, int red, int green, int blue)
+        : RGBColor(red, green, blue),
+          mName(name),
+          mInitialX(initialX),
+          mInitialY(initialY),
+          mSpeedX(speedX),
+          mSpeedY(speedY) {}
+};
+
+class Circle : public CShape
+{
+public:
+    float mRadius;
+
+    Circle(std::string name, float initialX, float initialY, float speedX, float speedY, int red, int green, int blue, float radius)
+        : CShape(name, initialX, initialY, speedX, speedY, red, green, blue),
+          mRadius(radius) {}
+};
+
+class Rectangle : public CShape
+{
+public:
+    float mWidth;
+    float mHeight;
+
+    Rectangle(std::string name, float initialX, float initialY, float speedX, float speedY, int red, int green, int blue, float width, float height)
+        : CShape(name, initialX, initialY, speedX, speedY, red, green, blue),
+          mWidth(width),
+          mHeight(height) {}
+};
+
+class GameWindow
+{
+public:
+    int mWidth;
+    int mHeight;
+
+    GameWindow(){};
+    GameWindow(int width, int height) : mWidth(width),
+                                        mHeight(height) {}
+};
+
+class FontConfig : RGBColor
+{
+public:
+    std::string mFontFile;
+    int mSize;
+
+    FontConfig(){};
+    FontConfig(int red, int green, int blue, std::string fontFile, int size) : RGBColor(red, green, blue),
+                                                                               mFontFile(fontFile),
+                                                                               mSize(size) {}
+};
+
+void parseConfig(std::string filename, std::vector<CShape> &shapes, GameWindow &gameWindow, FontConfig &fontConfig)
+{
+    float initialX, initialY, speedX, speedY, radius;
+    int width, height, r, g, b, size;
+    std::string temp;
+    std::ifstream fileIn(filename);
+
+    while (fileIn >> temp)
+    {
+        if (temp == "Window")
+        {
+            fileIn >> width >> height;
+            gameWindow = GameWindow(width, height);
+        }
+        else if (temp == "Font")
+        {
+            fileIn >> temp >> size >> r >> g >> b;
+            fontConfig = FontConfig(r, g, b, temp, size);
+        }
+        else if (temp == "Circle")
+        {
+            fileIn >> temp >> initialX >> initialY >> speedX >> speedY >> r >> g >> b >> radius;
+            Circle newCircle = Circle(temp, initialX, initialY, speedX, speedY, r, g, b, radius);
+            newCircle.mSfmlCircle = sf::CircleShape(radius, DEFAULT_SEGMENTS);
+            newCircle.mSfmlCircle.setFillColor(sf::Color(r, g, b));
+            newCircle.mSfmlCircle.setPosition(initialX, initialY);
+            shapes.push_back(newCircle);
+        }
+        else if (temp == "Rectangle")
+        {
+            fileIn >> temp >> initialX >> initialY >> speedX >> speedY >> r >> g >> b >> width >> height;
+            Rectangle newRectangle = Rectangle(temp, initialX, initialY, speedX, speedY, r, g, b, width, height);
+            newRectangle.mSfmlRectangle = sf::RectangleShape(sf::Vector2f(height, width));
+            newRectangle.mSfmlRectangle.setFillColor(sf::Color(r, g, b));
+            newRectangle.mSfmlRectangle.setPosition(initialX, initialY);
+            shapes.push_back(newRectangle);
+        }
+    }
+};
+
+float checkXBounds(GameWindow &gameWindow, sf::FloatRect &globalBounds)
+{
+    if (globalBounds.left < 0 || globalBounds.left + globalBounds.width > gameWindow.mWidth)
+    {
+        return -1;
+    }
+    return 1;
+}
+
+float checkYBounds(GameWindow &gameWindow, sf::FloatRect &globalBounds)
+{
+    if (globalBounds.top < 0 || globalBounds.top + globalBounds.height > gameWindow.mHeight)
+    {
+        return -1;
+    }
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
-    // Create new window w*h, needs to be read from config file
-    const int wWidth = 1280;
-    const int wHeight = 720;
-    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML works!");
-    window.setFramerateLimit(60);
+    std::vector<CShape> shapes;
+    std::vector<sf::Shape> sfShapes;
+    GameWindow gameWindow;
+    FontConfig fontConfig;
+    parseConfig("config.txt", shapes, gameWindow, fontConfig);
+
+    // Create new window
+    const int wWidth = gameWindow.mWidth;
+    const int wHeight = gameWindow.mHeight;
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "Assignment 1");
+    window.setFramerateLimit(FRAMERATE);
 
     // Init IMGUI and create a clock used for its internal timing
     ImGui::SFML::Init(window);
     sf::Clock deltaClock;
-
     // Scale the IMGUI UI by a factor
     ImGui::GetStyle().ScaleAllSizes(1.0f);
 
-    float c[3] = {0.0f, 1.0f, 1.0f};
+    sf::Font myFont;
 
-    // Configure shape
-    float cRadius = 50;
-    int cSegments = 32;
-    float cSpeedX = 1.0f;
-    float cSpeedY = 0.5f;
-    bool drawCircle = true;
-    bool drawText = true;
-
-    sf::CircleShape circle(cRadius, cSegments); // Create SFML circle shape
-    circle.setPosition(10.0f, 10.0f);           // Set the top-left position
-
-    /* sf::Font myFont;
-
-    if (!myFont.loadFromFile("fonts/tech.ttf"))
+    if (!myFont.loadFromFile(fontConfig.mFontFile))
     {
         std::cerr << "Could not load font!" << std::endl;
         exit(-1);
     }
 
     sf::Text text("Sample Text", myFont, 24);
-    text.setPosition(0, wHeight - (float)text.getCharacterSize()); */
+    text.setPosition(0, wHeight - (float)text.getCharacterSize());
 
     char displayString[255] = "Sample Text";
 
@@ -60,22 +194,13 @@ int main(int argc, char *argv[])
             {
                 window.close();
             }
-
-            if (event.type == sf::Event::KeyPressed)
-            {
-                std::cout << "Key pressed with code = " << event.key.code << std::endl;
-
-                if (event.key.code == sf::Keyboard::X)
-                {
-                    cSpeedX *= -1.0f;
-                }
-            }
         }
 
         ImGui::SFML::Update(window, deltaClock.restart());
+        // drawUI();
 
         // Draw UI
-        ImGui::Begin("Window title");
+        /* ImGui::Begin("Window title");
         ImGui::Text("Window text!");
         ImGui::Checkbox("Draw Circle", &drawCircle);
         ImGui::SameLine();
@@ -84,35 +209,41 @@ int main(int argc, char *argv[])
         ImGui::SliderInt("Sides", &cSegments, 3, 64);
         ImGui::ColorEdit3("Color Circle", c);
         ImGui::InputText("Text", displayString, 255);
-        /* if (ImGui::Button("Set Text"))
+        if (ImGui::Button("Set Text"))
         {
             text.setString(displayString);
-        } */
+        }
         ImGui::SameLine();
         if (ImGui::Button("Reset Circle"))
         {
             circle.setPosition(0, 0);
         }
-        ImGui::End();
-
-        // Set circle properties after UI updated them
-        circle.setFillColor(sf::Color(c[0] * 255, c[1] * 255, c[2] * 255));
-        circle.setPointCount(cSegments);
-        circle.setRadius(cRadius);
-
-        // Basic animation
-        circle.setPosition(circle.getPosition().x + cSpeedX, circle.getPosition().y + cSpeedY);
+        ImGui::End(); */
 
         // Basic rendering
         window.clear();
-        if (drawCircle)
+
+        for (auto &shape : shapes)
         {
-            window.draw(circle);
+            if (shape.mName.rfind("C", 0) == 0)
+            {
+                sf::Vector2<float> currentPosition = shape.mSfmlCircle.getPosition();
+                sf::FloatRect globalBounds = shape.mSfmlCircle.getGlobalBounds();
+                shape.mSpeedX *= checkXBounds(gameWindow, globalBounds);
+                shape.mSpeedY *= checkYBounds(gameWindow, globalBounds);
+                shape.mSfmlCircle.setPosition(currentPosition.x + shape.mSpeedX, currentPosition.y + shape.mSpeedY);
+                window.draw(shape.mSfmlCircle);
+            }
+            else
+            {
+                sf::Vector2<float> currentPosition = shape.mSfmlRectangle.getPosition();
+                sf::FloatRect globalBounds = shape.mSfmlRectangle.getGlobalBounds();
+                shape.mSpeedX *= checkXBounds(gameWindow, globalBounds);
+                shape.mSpeedY *= checkYBounds(gameWindow, globalBounds);
+                shape.mSfmlRectangle.setPosition(currentPosition.x + shape.mSpeedX, currentPosition.y + shape.mSpeedY);
+                window.draw(shape.mSfmlRectangle);
+            }
         }
-        /* if (drawText)
-        {
-            window.draw(text);
-        } */
 
         ImGui::SFML::Render(window); // Draw the UI last so it's on top
         window.display();            // Display window
